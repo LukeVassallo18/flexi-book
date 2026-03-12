ab2934faed5281ef5ee07b9298c9e05149dbd412
LukeVassallo18 <lukevas189@gmail.com>
2026-03-12 09:45:10 +0100

    created basic standard colour scheme

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+import Navbar from './components/Navbar/Navbar.vue';
 </script>
 
 <template>
   <div id="app">
+    <Navbar />
   </div>
 </template>
 
---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <nav class="navbar">
+    <div class="navbar-container">
+      <a href="#" class="navbar-logo">MyApp</a>
+      <ul class="navbar-menu">
+        <li><a href="#">Flights</a></li>
+        <li><a href="#">Hotels</a></li>
+        <li><a href="#">Car Rentals</a></li>
+        <li>
+          <a href="#" class="navbar-link with-icon">
+            <span class="material-icons" aria-hidden="true">accessibility_new</span>
+            <span>Accessibility</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped src="./Navbar.css"></style>
